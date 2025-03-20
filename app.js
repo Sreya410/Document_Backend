017868c5f29f180a2bb98b3588cda2d52a186f5b
Sreya410 <sreya3492@gmail.com>
2025-03-20 09:54:20 +0530

    Commit

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(cors());
+app.use(cors({origin: "https://document-editor-murex.vercel.app"}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
